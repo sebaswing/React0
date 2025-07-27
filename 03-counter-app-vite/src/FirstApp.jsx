fedb25305a7b62bab462edb45cb9fc5c164bde14
Juan Sebastián Beltrán Leal <beltry1@hotmail.com>
2025-07-27 17:44:42 -0300

    update for firstApp component

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,9 +1,14 @@
 // import { Fragment } from "react";
+// const GetName = (a,b) => {
+//     return a + b;
+// }
+
 export const FirstApp = () => {
     return (
         // <Fragment>
-        //es lo mismo que un Fragment
+        //<>   es lo mismo que un Fragment
         <> 
+            {/* <h1>{ GetName(2,3) }</h1> */}
             <h1>Sebastián</h1>
             <p>soy un subtitulo</p>
         </>
