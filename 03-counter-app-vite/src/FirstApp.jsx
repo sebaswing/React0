a00fa524c79ba387de0dbdc727e252ac00124d2b
Juan Sebastián Beltrán Leal <beltry1@hotmail.com>
2025-07-27 16:49:45 -0300

    adding vite devs new app and fragment

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,12 @@
+// import { Fragment } from "react";
+export const FirstApp = () => {
+    return (
+        // <Fragment>
+        //es lo mismo que un Fragment
+        <> 
+            <h1>Sebastián</h1>
+            <p>soy un subtitulo</p>
+        </>
+        // </Fragment>
+    );
+}
---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,13 @@
+
+// export function App(){
+//     //document.createElement....
+//     return <h1>Hola Mundo!!</h1>;
+// }
+
+export const HelloWorldApp = () => {
+    return( 
+        <h1>Hola Mundo!!</h1>
+    );
+}
+
+//FirstApp
