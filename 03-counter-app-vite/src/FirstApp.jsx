950c9e65a18131a57c5f663948db9caaf0a21e6c
Juan Sebastián Beltrán Leal <beltry1@hotmail.com>
2025-08-10 18:38:12 -0300

    default props

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -5,7 +5,7 @@
 
 import PropTypes from 'prop-types';
 
-export const FirstApp = ({title, subtitle}) => {
+export const FirstApp = ({ title, subtitle,name }) => {
 
 // if (!title) {
 //     throw new Error("El título no existe");
@@ -19,6 +19,7 @@ export const FirstApp = ({title, subtitle}) => {
             {/* <h1>{ GetName(2,3) }</h1> */}
             <h1>{title}</h1>
             <p>{subtitle}</p>
+            <p>{name}</p>
         </>
         // </Fragment>
     );
@@ -26,5 +27,11 @@ export const FirstApp = ({title, subtitle}) => {
 
 FirstApp.propTypes = {
     title: PropTypes.string.isRequired,
-    subtitle: PropTypes.number,
+    subtitle: PropTypes.string,
+}
+
+FirstApp.defaultProps = {
+    name: 'Sebastián Beltrán', // Propiedad por defecto,
+    subtitle: 'No hay subtítulo',
+    title: 'No hay título',
 }
