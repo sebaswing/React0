2c4a52d8de3474bccd886fbca6095dba65a2a261
Juan Sebastián Beltrán Leal <beltry1@hotmail.com>
2025-09-28 14:02:03 -0300

    adding Styles

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,15 @@
 export const GifExpertApp = () => {
   return (
     <>
+    {/*titulo  */}
       <h1>GifExpertApp</h1>
+
+      {/*input */} 
+
+
+      {/*listado de gifs */} 
+       {/*gif item */}
+
     </>
   )
 }
