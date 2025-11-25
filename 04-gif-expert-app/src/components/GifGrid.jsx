103f5da6b5e1694316486341a3a5cf5ff1d81fbc
Juan Sebastián Beltrán Leal <beltry1@hotmail.com>
2025-11-25 11:53:36 -0300

    adding helper

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,14 +1,17 @@
 import {getGifs} from "../helpers/getGifs";
+import { useState } from "react";
 
 export const GifGrid = ({category}) => {
 
+  const [counter, setCounter] = useState(10);
   // getGifs(category);
   console.log(getGifs(category));
 
   return (
    <>
        <h3>{category}</h3>
-        
+      <h5>{counter}</h5>
+      <button onClick={ () => setCounter(counter + 1)}>+1</button>
    </>
   )
 }
--- 04-gif-expert-app/src/helpers/getGifs.js
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
   const api_key = import.meta.env.VITE_API_KEY;
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${category}&limit=5`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${category}&limit=10`;
   const resp = await fetch(url);
   const { data } = await resp.json();
   const gifs = data.map((img) => ({
