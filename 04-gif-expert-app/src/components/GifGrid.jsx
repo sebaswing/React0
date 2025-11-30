4f38a981cdfe74c2528e2186c683e0035ceea1d4
Juan Sebastián Beltrán Leal <beltry1@hotmail.com>
2025-11-30 17:53:54 -0300

    useEffect hook + .gitignore update

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,15 +1,21 @@
 import {getGifs} from "../helpers/getGifs";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const GifGrid = ({category}) => {
 
   const [counter, setCounter] = useState(10);
-  // getGifs(category);
-  console.log(getGifs(category));
+
+  useEffect(()=>{
+      getGifs(category);
+      // console.log(getGifs(category));
+      
+  },[])
+
+
 
   return (
    <>
-       <h3>{category}</h3>
+      <h3>{category}</h3>
       <h5>{counter}</h5>
       <button onClick={ () => setCounter(counter + 1)}>+1</button>
    </>
