79e8ea79194b9d72d64a211c44692382a1b51a99
Juan Sebastián Beltrán Leal <beltry1@hotmail.com>
2026-01-05 16:40:52 -0300

    adding getgifs test

--- 04-gif-expert-app/test/helpers/getGifs.test.js
+++ 04-gif-expert-app/test/helpers/getGifs.test.js
@@ -1,14 +1,15 @@
 import { getGifs } from "../../src/helpers/getGifs";
-import { getKey } from "../../src/configu/getKey";
-describe('pruebas en getGifs()', () => {
-  test('imprime los gifs', async () => {
-    const gifs = await getGifs('One Punch',getKey());
+import { getKey } from "../../src/configu/getKey"; // create a key to test with your own key
+
+describe("pruebas en getGifs()", () => {
+  test("imprime los gifs", async () => {
+    const gifs = await getGifs("One Punch", getKey());
     // console.log(gifs);// .log para ver los gifs
     expect(gifs.length).toBeGreaterThan(0);
     expect(gifs[0]).toEqual({
-      id:expect.any(String),
-      title:expect.any(String),
-      url:expect.any(String),
-    })
+      id: expect.any(String),
+      title: expect.any(String),
+      url: expect.any(String),
+    });
   });
 });
