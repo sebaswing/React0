636e0afe89c056cce0a0f77fef4de51fed9b2bc3
Juan Sebastián Beltrán Leal <beltry1@hotmail.com>
2026-01-11 19:08:28 -0300

    add categories submit test

--- 04-gif-expert-app/test/components/AddCategory.test.js
+++ 04-gif-expert-app/test/components/AddCategory.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render,screen } from "@testing-library/react";
+import { fireEvent, getByRole, render,screen } from "@testing-library/react";
 import { AddCategory } from "../../src/components/AddCategory";
 
 
@@ -17,16 +17,34 @@ describe('Purebas en <AddCategory/>', () => {
     test('debe llamar onNewCategory si el input tiene un valor', () => { // prueba el submit 
         
         const inputValue = 'Gokú';
-        //TODO
-        render(<AddCategory onNewCategory={() => {}} />);
+        const onNewCategory = jest.fn(); //simula la funcion para ver si se llama
+
+        render(<AddCategory onNewCategory={onNewCategory} />);
 
         const input = screen.getByRole('textbox');
         const form = screen.getByRole('form');
-        fireEvent.input(input, {target: {value: inputValue}});
+        fireEvent.input(input, {target: {value: inputValue}});//cambia el valor del input
         fireEvent.submit (form);
-        screen.debug();
+        // screen.debug();
 
         expect(input.value).toBe(''); //verifica que el input se haya limpiado
-     })
+        
+        // expect(onNewCategory).toHaveBeenCalled(); //verifica que se haya llamado la funcion
+        // expect(onNewCategory).toHaveBeenCalledTimes(1); //verifica que se haya llamado 1 vez
+        expect(onNewCategory).toHaveBeenCalledWith(inputValue); //verifica que se haya llamado con el valor correcto
+    
+    })
+    test('no debe llamar onNewCategory si el input esta vacio', () => { // prueba el submit
+
+        const inputValue = '';
+        const onNewCategory = jest.fn()
+        render(<AddCategory onNewCategory={onNewCategory} />);
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+        fireEvent.input(input, {target: {value: inputValue}});
+        fireEvent.submit(form);
+        expect(onNewCategory).toHaveBeenCalledTimes(0);
+        
+    })
 
 });
