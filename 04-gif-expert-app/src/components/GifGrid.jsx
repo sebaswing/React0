8c23d4b3d2847aa4cf82f666c9e5823a51b3efa0
Juan Sebastián Beltrán Leal <beltry1@hotmail.com>
2026-01-11 20:03:48 -0300

    continue testgrid

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -2,6 +2,7 @@
 import { useState,useEffect } from "react";
 import { GifItem } from "./GifItem";
 import { useFetchGifs } from "../hooks/useFetchGifs";
+import PropTypes from "prop-types";
 
 export const GifGrid = ({category}) => {
 
@@ -32,3 +33,8 @@ export const GifGrid = ({category}) => {
    </>
   )
 }
+
+GifGrid.propTypes = {
+  category: PropTypes.string.isRequired,
+
+};
--- 04-gif-expert-app/test/components/GifGrid.test.js
+++ 04-gif-expert-app/test/components/GifGrid.test.js
@@ -1,11 +1,19 @@
-import { render } from "@testing-library/react";
+import { render,screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
 
 describe('Pruebas en el <GifGrid/>', () => { 
 
     const category = 'One Punch';
     test('debe de mostrar el loading inicialmente', () => {
-        render(<GifGrid/>);
+        render(<GifGrid  category={category} />);
+        screen.debug();
+        expect( screen.getByText('Cargando...'));
+        expect( screen.getByText(category) );
+
+    });
+
+    test('debe de mostrar items cuando se cargan las imagenes useFetchGifs', () => {
+        
     });
     
  });
